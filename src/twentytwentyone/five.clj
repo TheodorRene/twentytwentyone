d2514629f3d2f1a4b384459a5c5f2ddee6f3989e
Theodor René Carlsen <theodor.rene@gmail.com>
2021-12-09 23:33:50 +0100

    day 5 task2 finally

--- src/twentytwentyone/five.clj
+++ src/twentytwentyone/five.clj
@@ -33,21 +33,43 @@
   )
 )
 
-(gen-points [[1 1] [3 3]])
-(gen-points [[3 3] [1 1]])
-(gen-points [[3 1] [1 3]])
+
+(defn gen-points-diag-helper [list-of-points end-point new-point incr]
+  (let [[x y] new-point
+        next-point [(+ x 1) (+ y incr)]
+        ]
+  (if (= end-point new-point)
+    (conj list-of-points new-point)
+    (gen-points-diag-helper (conj list-of-points new-point) end-point next-point incr)
+    )
+  )
+  )
+
 
 (defn gen-points-diag [line]
   (let [ [x1 y1 x2 y2](flatten line)
-         [x_smol x_big] (sort [x1 x2])
-         [y_smol y_big] (sort [y1 y2])
-        ]
+         [y_smol _] (sort [y1 y2])
+         ]
+         (if(= y_smol y1)
+           (gen-points-diag-helper [] [x2 y2] [x1 y1] 1) ;; upwards /
+           (gen-points-diag-helper [] [x2 y2] [x1 y1] -1) ;; downwards \
+          )
   )
+)
 
-(gen-points [[1 1][1 3]])
-(gen-points [[1 3][1 1]])
-(gen-points [[9 7] [7 7]])
-(gen-points [[0 9] [5 9]])
+(defn final-gen-points-diag [line]
+  (let [ [x1 y1 x2 y2] (flatten line)
+         positive-x (if (> x2 x1) [[x1 y1] [x2 y2]] [[x2 y2] [x1 y1]])
+         ]
+    (gen-points-diag positive-x)
+  )
+)
+(defn generate-points [line]
+  (if (is-v-or-h? line)
+    (gen-points line)
+    (final-gen-points-diag line)
+  )
+)
 
 (defn single-point-to-sin [p]
   (let [[x y] p]
@@ -55,10 +77,9 @@
   )
 )
 
-(def path-test "./resources/five_text.txt")
-(def path-carl "./resources/five_carl.txt")
 (def path "./resources/five.txt")
 
+;; Part 1
 (->> path
      (slurp)
      (str/split-lines)
@@ -73,4 +94,20 @@
      (filter #(<= 2 %))
      (count)
 )
+
+;; Part 2
+(->> path
+     (slurp)
+     (str/split-lines)
+     (map #(str/split % #"\-\>"))
+     (map parse-line) ;; [(x1,y1), (x2, y2)]
+     (map generate-points) 
+     (mapcat identity) ;; Flatten one level -> [(x,y)]
+     (map single-point-to-sin)
+     (frequencies)
+     (vals)
+     (filter #(<= 2 %))
+     (count)
+)
+
 
